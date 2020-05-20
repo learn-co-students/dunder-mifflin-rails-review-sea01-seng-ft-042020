305f155d7d90ec56c2d97f75885fdd92668af43a
Trevor <tlow94@uw.edu>
2020-05-20 16:12:01 -0700

    Done.

--- app/controllers/dogs_controller.rb
+++ app/controllers/dogs_controller.rb
@@ -1,2 +1,17 @@
 class DogsController < ApplicationController
+
+    def index
+        @dogs = Dog.all
+    end
+
+    def new
+    end
+
+    def create
+    end
+
+    def show
+        @dog = Dog.find(params[:id])
+    end
+
 end
